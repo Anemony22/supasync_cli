b07ea6c0a0f12aa5b0b63b4e082ced362038bf7d
Ben Clark <bennyjc01@gmail.com>
2024-07-03 22:04:40 +0800

    Fixed error help printout

--- bin/supasync_cli.dart
+++ bin/supasync_cli.dart
@@ -4,21 +4,14 @@ import 'package:args/command_runner.dart';
 
 import 'package:supasync_cli/supasync_cli.dart';
 
-void printUsage(CommandRunner argParser) {
-  print('Usage: dart supasync_cli.dart <flags> [arguments]');
-  print(argParser.usage);
-}
-
 void main(List<String> args) {
   var runner = CommandRunner('supasync', 'A CLI tool to streamline local development with Supabase and PowerSync.')..addCommand(TestCommand());
 
-  runner.argParser.addFlag('verbose', abbr: 'v', help: 'Displays extra logging information for a command.');
+  runner.argParser.addFlag('verbose', abbr: 'v', negatable: false, help: 'Displays extra logging information for a command.');
 
   runner.run(args).catchError((error) {
     if (error is! UsageException) throw error;
     print(error);
-    print('');
-    printUsage(runner);
     exit(64); // Exit code 64 indicates a usage error.
   });
 }
